a309cc066449ccc418c8e7b2197f3a90793f1103
Leandro Bitencourt <leandrocintrabitencourt@gmail.com>
2019-03-13 02:00:51 -0300

    Commit initial

---
+++ run.py
@@ -0,0 +1,99 @@
+import socket
+import socks
+import pika
+import re
+# After we configured socks5
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+# from multiprocessing import Pool
+
+
+def is_url(link):
+    # checks using regex if 'link' is a valid url
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*/\\,() ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', link)
+    return " ".join(url) == link
+
+
+def create_connection(addr, timeout=None, source_address=None):
+    sock = socks.socksocket()
+    sock.connect(addr)
+    return sock
+
+
+def set_proxy():
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9150, True)
+    socket.socket = socks.socksocket
+    socket.create_connection = create_connection
+
+'''
+
+def produce_message(message, i):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.exchange_declare(exchange='web_crawler', exchange_type='direct')
+    channel.basic_publish(exchange='web_crawler',
+                          routing_key='crawler_{}'.format(i),
+                          body=message,
+                          properties=pika.BasicProperties(delivery_mode=2))
+    connection.close()
+
+
+def new_message(ch, method, properties, body):
+    print('New message: {}'.format(body))
+
+
+def consume_message(i):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.exchange_declare(exchange='web_crawler', exchange_type='direct')
+    channel.queue_bind(exchange='web_crawler',
+                      queue='task_queue',
+                      routing_key='crawler_{}'.format(i))
+    channel.basic_consume(new_message,
+                          queue='task_queue',
+                          no_ack=True)
+    channel.start_consuming()
+    
+ for i in range(2):
+    message = 'http://check.torproject.org?key={}'.format(i)
+    produce_message(message, i)
+
+pool = Pool(processes=40)
+pool.map(start_crawler)
+
+ '''
+
+
+listUrl = []
+
+
+def read_url(url, depth):
+    if depth == 5:
+        return url
+    else:
+        page = urlopen(url)
+        soup = BeautifulSoup(page.read(), 'html.parser')
+        links = soup.find_all('a')
+        if links is None or len(links) == 0:
+            listUrl.append(url)
+            return 1;
+        else:
+
+            for link in links:
+                if is_url(link['href']):
+                    if link['href'] not in listUrl:
+                        print('{} | {}'.format(url, soup.title.get_text()))
+                        listUrl.append(url)
+                        read_url(link['href'], depth+1)
+
+
+def start_crawler():
+    # consume_message(i)
+    read_url('http://torlinkbgs6aabns.onion/', 0)
+
+
+if __name__ == '__main__':
+    set_proxy()
+    start_crawler()
